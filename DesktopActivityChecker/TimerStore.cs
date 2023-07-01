2df03c5fc14f95ebabb7a0c6b1d055f7d133c0a5
Anand Phulwani <anand_phulwani@yahoo.com>
2023-07-01 15:24:46 +0530

    Create TimerStore.cs

---
+++ DesktopActivityChecker/TimerStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopActivityChecker
+{
+    public class TimerStore
+    {
+        private Dictionary<string, Timer> timersDict = new Dictionary<string, Timer>();
+
+        public void AddTimer(string key, Timer timer)
+        {
+            timersDict[key] = timer;
+        }
+
+        public Timer GetTimer(string key)
+        {
+            if (timersDict.TryGetValue(key, out Timer timer))
+            {
+                return timer;
+            }
+            return null;
+        }
+
+        public void RemoveTimer(string key)
+        {
+            timersDict.Remove(key);
+        }
+        // Other methods as needed...
+    }
+}
