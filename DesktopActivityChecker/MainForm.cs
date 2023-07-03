3ee17df67c500167287c786c31fe4c32e7210f93
Anand Phulwani <anand_phulwani@yahoo.com>
2023-07-03 19:55:44 +0530

    Removed duplicates from colors array if any.

--- DesktopActivityChecker/MainForm.cs
+++ DesktopActivityChecker/MainForm.cs
@@ -1076,7 +1076,7 @@ namespace DesktopActivityChecker
                     int noOfValidCaptures = 0;
                     for (int i = 0; i < Convert.ToInt32(formData.CapturePerInterval); i++)
                     {
-                        string[] colors = formData.ComparisonValue.Split(',');
+                        string[] colors = formData.ComparisonValue.Split(',').Distinct().ToArray();
                         int timeout = Convert.ToInt32(formData.SleepBetweenCaptures) > 200 ? (Convert.ToInt32(formData.SleepBetweenCaptures) / 2 > 2000 ? Convert.ToInt32(formData.SleepBetweenCaptures) - 2000 : Convert.ToInt32(formData.SleepBetweenCaptures) / 2) : 100;
                         Image newImage = getImageFromCoordinatesOfFormData(formData, timeout);
 
